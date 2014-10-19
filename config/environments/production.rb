7ddf9d216afe5391052c275058d3a5b44cebe8c9
youkei <youxkei@gmail.com>
2014-10-19 17:55:39 +0900

    Send email via gmail in production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,14 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: "snapsaver.levelop.org" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    authentication: :plain,
+    user_name: ENV["GMAIL_USER_NAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
 end
