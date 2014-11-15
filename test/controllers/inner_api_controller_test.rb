f80722b059b0f0f25718be5d8330fa115fbedb8f
youkei <youxkei@gmail.com>
2014-11-15 18:25:19 +0900

    Write InnerAPIController test

--- test/controllers/inner_api_controller_test.rb
+++ test/controllers/inner_api_controller_test.rb
@@ -1,4 +1,5 @@
 require 'test_helper'
+require 'fileutils'
 
 class InnerApiControllerTest < ActionController::TestCase
   include Devise::TestHelpers
@@ -10,7 +11,15 @@ class InnerApiControllerTest < ActionController::TestCase
     test "make_id" do
       post :make_id
 
-      assert_response :bad_request
+      assert_response :redirect
+      assert_equal UrlList.count, 1
+
+      name = UrlList.first.name
+
+      assert_match(/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/, name)
+      assert_nothing_raised do
+        delete_bitbucket_repository name
+      end
     end
 
     test "save_urls" do
@@ -48,38 +57,95 @@ class InnerApiControllerTest < ActionController::TestCase
       post :save_urls, urls: "http://www.example.com\nhttps://www.google.com\nfile:///etc/passwd"
 
       assert_response :success
-      assert_equal URLList.find_by(name: FactoryGirl.attributes_for(:url_list)[:name]), "http://www.example.com\nhttps://www.google.com"
+      assert_equal UrlList.find_by(name: FactoryGirl.attributes_for(:url_list)[:name]), "http://www.example.com\nhttps://www.google.com"
     end
 
     test "shoot" do
-      Dir.mkdir "repo/"
+      Dir.mkdir "repo/hoge_list"
+
+      post :save_urls, id: "hoge_list", index: "-1", breakpoint: "lg"
+      assert_response :bad_request
+
+      post :save_urls, id: "hoge_list", index: "0", breakpoint: "lg"
+      assert_response :success
+
+      post :save_urls, id: "hoge_list", index: "1", breakpoint: "lg"
+      assert_response :success
+
+      post :save_urls, id: "hoge_list", index: "2", breakpoint: "lg"
+      assert_response :bad_request
+
+      FileUtils.rm_rf "repo/hoge_list"
     end
 
     test "push_repository" do
+      Dir.mkdir "repo/hoge_list"
+
+      post :push_repository
+      assert_response :bad_request
+
+      File.open "repo/hoge_list/piyo", "w" do end
+
+      post :push_repository
+      assert_response :success
+
+      FileUtils.rm_rf "repo/hoge_list"
     end
   end
 
   sub_test_case "ログイン時" do
     setup do
-      sign_in FactoryGirl.build(:user)
+      user = FactoryGirl.create(:user)
+      url_list = FactoryGirl.create(:url_list)
+
+      url_list.user = user.id
+
+      sign_in user
     end
 
     test "make_id" do
-      sign_in FactoryGirl.build(:user)
-
       post :make_id
 
-      assert_response :redirect
-      assert_equal URLList.count, 1
+      assert_response :bad_request
+    end
 
-      name = URLList.first.name
+    test "save_urls" do
+      post :save_urls, urls: "http://www.example.com\nhttps://www.google.com\nfile:///etc/passwd"
 
-      assert_match(/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/, name)
-      assert_nothing_raised do
-        delete_bitbucket_repository name
-      end
+      assert_response :success
+      assert_equal User.find_by(email: FactoryGirl.attributes_for(:user)[:email]).url_lists.find_by(name: FactoryGirl.attributes_for(:url_list)[:name]), "http://www.example.com\nhttps://www.google.com"
+    end
+
+    test "shoot" do
+      Dir.mkdir "repo/this-is-uuid-hoge_list"
+
+      post :save_urls, id: "hoge_list", index: "-1", breakpoint: "lg"
+      assert_response :bad_request
+
+      post :save_urls, id: "hoge_list", index: "0", breakpoint: "lg"
+      assert_response :success
+
+      post :save_urls, id: "hoge_list", index: "1", breakpoint: "lg"
+      assert_response :success
+
+      post :save_urls, id: "hoge_list", index: "2", breakpoint: "lg"
+      assert_response :bad_request
+
+      FileUtils.rm_rf "repo/this-is-uuid-hoge_list"
+    end
+
+    test "push_repository" do
+      Dir.mkdir "repo/this-is-uuid-hoge_list"
+
+      post :push_repository
+      assert_response :bad_request
+
+      File.open "repo/this-is-uuid-hoge_list/piyo", "w" do end
+
+      post :push_repository
+      assert_response :success
+
+      FileUtils.rm_rf "repo/this-is-uuid-hoge_list"
     end
   end
-
-
 end
--- test/factories/users.rb
+++ test/factories/users.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :user do
     email "hoge@example.com"
+    uuid "this-is-uuid"
     password "password"
     password_confirmation "password"
   end
